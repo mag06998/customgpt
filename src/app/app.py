d40ee81188b1fe2d608ede1de469e255ca27e355
Matt <mag06998@gmail.com>
2025-07-11 16:37:25 -0400

    Added front end folder. Added functionality to be able to serve angular fronend:
    returns output of send_from_directory(str(angular_dist_path),"index.html") when base path is selected;
    
    as well as added functionality to handle when angular requests backend for additional resources once the initial "index.html" has been served up. @app.route("/<path:path>") decorator added to function to serve up these additional resources; path variable fed to function. If resource doesn't exist then function routes back to "index.html" like the base route.

--- src/app/app.py
+++ src/app/app.py
@@ -3,7 +3,9 @@
 
 """
 
-from flask import Flask
+from flask import Flask, send_from_directory
+from pathlib import Path
+
 from src.app.routes import chat_blueprint
 from flask_cors import CORS
 
@@ -16,10 +18,24 @@ def create_app():
     CORS(app, origins=[
         "http://localhost:4200"])  # delete in production; this is just allowing cross origin communication for angular dev server
     app.register_blueprint(chat_blueprint,url_prefix='/chat')
-    @app.route('/')
+
+    angular_dist_path=Path(__file__).parent.parent.parent / "frontend" / "dist" / "custom-gpt-frontend" / "browser"
+    print(angular_dist_path)
+    @app.route('/',methods=['GET'])
     def index():
         # this will eventually have to return angular application
-        return "Hello app is running!"
+        #'frontend//dist//custom-gpt-frontend//browser//index.html'
+        return send_from_directory(str(angular_dist_path),"index.html")
+
+    @app.route('/<path:path>')
+    def static_files(path):
+        #needs to serve up other files needed
+        full_path = angular_dist_path / path
+        print(full_path)
+        if full_path.exists():
+            return send_from_directory(str(angular_dist_path),path)
+        else:
+            return send_from_directory(str(angular_dist_path),"index.html")
 
     return app
 
